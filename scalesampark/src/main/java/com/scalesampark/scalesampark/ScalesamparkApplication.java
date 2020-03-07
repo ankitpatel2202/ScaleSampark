9c975ab6e20ff82786ec4fc69839c9f81f655f7c
Ankit Patel <Ankit.Patel@nexusgroup.com>
2020-03-07 16:12:30 +0530

    Initial commit

---
+++ scalesampark/src/main/java/com/scalesampark/scalesampark/ScalesamparkApplication.java
@@ -0,0 +1,13 @@
+package com.scalesampark.scalesampark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalesamparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalesamparkApplication.class, args);
+	}
+
+}
---
+++ scalesampark/src/test/java/com/scalesampark/scalesampark/ScalesamparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalesampark.scalesampark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalesamparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
